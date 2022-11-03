374dc62c770138207217d5b77527d1702b17b26a
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-11-03 01:19:44 -0400

    @yoshubs explain this

--- source/ForeverTools.hx
+++ source/ForeverTools.hx
@@ -4,7 +4,6 @@ import flixel.FlxG;
 import flixel.system.FlxSound;
 import meta.data.*;
 import openfl.utils.Assets;
-import sys.FileSystem;
 
 /**
 	This class is used as an extension to many other forever engine stuffs, please don't delete it as it is not only exclusively used in forever engine
@@ -32,10 +31,10 @@ class ForeverTools
 			?defaultChangeableSkin:String = 'default', ?defaultBaseAsset:String = 'base'):String
 	{
 		var realAsset = '$baseLibrary/$assetModifier/$asset';
-		if (!FileSystem.exists(Paths.getPath('images/' + realAsset + '.png', IMAGE)))
+		if (!Assets.exists(Paths.getPath('images/' + realAsset + '.png', IMAGE)))
 		{
 			realAsset = '$baseLibrary/$assetModifier/$asset';
-			if (!FileSystem.exists(Paths.getPath('images/' + realAsset + '.png', IMAGE)))
+			if (!Assets.exists(Paths.getPath('images/' + realAsset + '.png', IMAGE)))
 				realAsset = '$baseLibrary/$defaultBaseAsset/$asset';
 		}
 
